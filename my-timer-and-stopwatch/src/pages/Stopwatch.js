bb11ab6bdfb08adaa4f9197a588d398fa55bc7a3
rnazmo <rnazmo@gmail.com>
2023-07-04 05:47:13 +0900

    implement very simple timer and stopwatch

--- my-timer-and-stopwatch/src/pages/Stopwatch.js
+++ my-timer-and-stopwatch/src/pages/Stopwatch.js
@@ -1,3 +1,54 @@
+import React, { useState, useEffect } from "react";
+
 export function Stopwatch() {
-  return <h1>Stopwatch</h1>;
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setMilliseconds((milliseconds) => milliseconds + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+    setMilliseconds(0);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time / 1000) % 60);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    return (
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(seconds).padStart(2, "0") +
+      "." +
+      String(milliseconds).padStart(2, "0")
+    );
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch App</h1>
+      <p>{formatTime(milliseconds)}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
+  );
 }
--- my-timer-and-stopwatch/src/pages/Timer.js
+++ my-timer-and-stopwatch/src/pages/Timer.js
@@ -1,3 +1,48 @@
+import React, { useState, useEffect } from "react";
+
 export function Timer() {
-  return <h1>Timer</h1>;
+  const [seconds, setSeconds] = useState(30);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (seconds === 0) {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, seconds]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+  };
+
+  const handleReset = () => {
+    setSeconds(30);
+  };
+
+  return (
+    <div>
+      <h1>Timer App</h1>
+      <p>{seconds}s</p>
+      {!isActive && seconds === 30 && (
+        <button onClick={handleStart}>Start</button>
+      )}
+      {isActive && <button onClick={handleStop}>Stop</button>}
+      {!isActive && seconds < 30 && (
+        <button onClick={handleReset}>Reset</button>
+      )}
+    </div>
+  );
 }
